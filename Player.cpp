7be44758c01e38d7fd8a5ae138eaaa317ac13a0a
gai <115063721+gai1637@users.noreply.github.com>
2023-10-04 12:40:04 +0900

    プロジェクト ファイルを追加します。

---
+++ Player.cpp
@@ -0,0 +1,15 @@
+#include "Player.h"
+#include "TextureManager.h"
+#include <cassert>
+void Player::Initialize() {
+	textureHandle_ = TextureManager::Load("mario.jpg");
+	sprite_ = Sprite::Create(textureHandle_, {100, 50});
+	model_ = Model::Create();
+	worldTransform_.Initialize();
+	viewProjection_.Initialize();
+}
+Player::~Player() {
+	delete sprite_;
+	delete model_;
+}
+void Player::Draw() { model_->Draw(worldTransform_, viewProjection_, textureHandle_); }
---
+++ Player.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+#include "Audio.h"
+#include "DirectXCommon.h"
+#include "Input.h"
+#include "Model.h"
+#include "SafeDelete.h"
+#include "Sprite.h"
+#include "ViewProjection.h"
+#include "WorldTransform.h"
+class Player {
+private:
+	uint32_t textureHandle_ = 0;
+	Sprite* sprite_ = nullptr;
+	Model* model_ = nullptr;
+	WorldTransform worldTransform_;
+	ViewProjection viewProjection_;
+
+public:
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~Player();
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 毎フレーム処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+};
